945ef208f883a29cd3f78ede60267a92e4c70d01
NattawatP <nattawat.puak@gmail.com>
2020-01-06 11:15:25 +0700

    Uploaded Mushroom Spawn Script

---
+++ Assets/Scripts/MushroomSpawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomSpawn : MonoBehaviour
+{
+    public int minX = 1;
+    public int maxX = 8;
+    public int minY = 3;
+    public int maxY = 8;
+    public int maxMushroom = 7;
+    public GameObject objectToSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RandomLocationGen();
+    }
+
+    void RandomLocationGen()
+    {
+        int count = 0;
+        while (count < maxMushroom)
+        {
+            int x = Random.Range(minX, maxX);
+            int y = Random.Range(minY, maxY);
+            Instantiate(objectToSpawn, new Vector3(x, y, 0), Quaternion.identity);
+            count++;
+        }
+    }
+}
