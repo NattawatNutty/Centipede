1248cc877f50429b549578f3af0347d67310b635
NattawatP <nattawat.puak@gmail.com>
2020-01-07 12:29:30 +0700

    Mushroom updated
    - Updated the code for mushroom properties.

---
+++ Assets/Scripts/Mushroom.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mushroom : MonoBehaviour
+{
+    public int mushroomLife = 3;    // Life point of a mushroom
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If HP <= 0, destroy the mushroom
+        if (this.mushroomLife <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        // Hit by a bullet will decrease the health point of mushroom
+        if (col.gameObject.CompareTag("Bullet"))
+        {
+            this.mushroomLife--;
+        }
+    }
+}
