910ec2244ffd4e623bd1f597eea9c26a558f1b75
NattawatP <nattawat.puak@gmail.com>
2020-01-07 12:30:38 +0700

    Player can fire now.
    - Added bullet prefab.
    - Added the script to enable the player to fire the bullets by clicking LMOUSE.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Destroy the bullet when it hits to reduce the memory
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,24 +4,50 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float cellSize = 0.5f;
-    public float bulletSpeed;       // Speed that a bullet can travel in cells per second
-    public int lives = 3;           // Lives given for the player (default = 3)
+    public float cellSize = 1f;
+    public float bulletSpeed = 5f;      // Speed that a bullet can travel in unit cells per second
+    public float fireRate = 0.25f;      // Fire rate for the bullet in seconds
+    public int lives = 3;               // Lives given for the player
+    public GameObject bulletPrefab;     // Prefab of a bullet
+
+    private float nextFire;             // Time the next bullet can be fired
+    private Rigidbody2D rb;             // Rigidbody of the player object
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") > 0)
+        Movement();
+        // If no fire rate
+        if (fireRate == 0 && Input.GetButtonDown("Fire1"))
+        {
+            FireBullet();
+        }
+        // Fire rate specified
+        else
+        {
+            if (Input.GetButton("Fire1") && Time.time > nextFire && fireRate > 0)
+            {
+                nextFire = Time.time + fireRate;
+                FireBullet();
+            }
+        }
+        CheckGameOver();
+    }
+
+    // Movement control function
+    void Movement()
+    {
+        if (Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") > 0)
         {
             transform.position += Vector3.up * cellSize;
         }
-        else if(Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") < 0)
+        else if (Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") < 0)
         {
             transform.position += Vector3.down * cellSize;
         }
@@ -35,8 +61,32 @@ public class Player : MonoBehaviour
         }
     }
 
+    // Fire a bullet from player
+    void FireBullet()
+    {
+        // Instantiate new bullet to fire from player
+        GameObject newBullet = Instantiate(bulletPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+        // Get Rigidbidy2D of the bullet to fire in up direction
+        Rigidbody2D bulletRb = newBullet.GetComponent<Rigidbody2D>();
+        bulletRb.velocity = Vector2.up * bulletSpeed;
+    }
+
+    // Detect collision with another Rigidbody2D
     void OnCollisionEnter2D(Collision2D col)
     {
-        Debug.Log("COL");
+        // Compare tag to detect whether the player collides with the centipede
+        if (col.gameObject.CompareTag("Centipede"))
+        {
+            lives--;
+        }
+    }
+
+    // Check the conditions whether it is game over
+    void CheckGameOver()
+    {
+        if (lives <= 0)
+        {
+
+        }
     }
 }
